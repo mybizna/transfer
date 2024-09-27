0f2d5b8cdb597836e040ffee730947cf0c754b40
Dedan <dedanirungu@gmail.com>
2024-09-27 10:13:29 +0300

    Changes to files: database/migrations/2024_08_03_085053_create_transfer_allowedin_table.php database/migrations/2024_08_03_085104_create_transfer_disallowedin_table.php database/migrations/2024_08_03_085114_create_transfer_blacklist_table.php database/migrations/2024_08_03_085120_create_transfer_whitelist_table.php database/migrations/2024_08_03_085128_create_transfer_transfer_table.php database/migrations/2024_09_03_140706_add_deleted_at_to_transfer_allowedin_table.php database/migrations/2024_09_03_140713_add_deleted_at_to_transfer_disallowedin_table.php database/migrations/2024_09_03_140726_add_deleted_at_to_transfer_blacklist_table.php database/migrations/2024_09_03_140735_add_deleted_at_to_transfer_whitelist_table.php database/migrations/2024_09_03_140744_add_deleted_at_to_transfer_transfer_table.php

--- database/migrations/2024_08_03_085128_create_transfer_transfer_table.php
+++ database/migrations/2024_08_03_085128_create_transfer_transfer_table.php
@@ -15,16 +15,21 @@ return new class extends Migration
             $table->id();
 
             $table->decimal('amount', 11)->nullable();
-            $table->foreignId('currency_id')->constrained('core_currency')->onDelete('cascade')->nullable()->index('transfer_transfer_currency_id');
+            $table->foreignId('currency_id')->nullable()->constrained('core_currency')->onDelete('set null');
             $table->string('token')->nullable();
             $table->longText('description')->nullable();
             $table->longText('params')->nullable();
             $table->boolean('is_canceled')->nullable()->default(false);
-            $table->foreignId('from_partner_id')->constrained('partner_partner')->onDelete('cascade')->nullable()->index('transfer_transfer_from_partner_id');
-            $table->foreignId('to_partner_id')->constrained('partner_partner')->onDelete('cascade')->nullable()->index('transfer_transfer_to_partner_id');
-            $table->foreignId('gateway_id')->constrained('account_gateway')->onDelete('cascade')->nullable()->index('transfer_transfer_gateway_id');
+            $table->foreignId('from_partner_id')->nullable()->constrained('partner_partner')->onDelete('set null');
+            $table->foreignId('to_partner_id')->nullable()->constrained('partner_partner')->onDelete('set null');
+            $table->foreignId('gateway_id')->nullable()->constrained('account_gateway')->onDelete('set null');
+
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->onDelete('set null');
 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
